ab54ead8ac2f255989172fa2128cfb4ef45cb616
Cara <cararbrennan@gmail.com>
2020-12-31 12:31:40 -0800

    Inital placement

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -6,7 +6,14 @@ function FrontPage() {
   return (
     <div className="FrontPage">
       <div className="FrontPageBody">
-        <h2 className="Title">Haiku Green</h2>
+        <div className="subscribe">
+          Subscribe
+        </div>
+        <div className="FrontPageText">
+          <h2 className="Title">Haiku Green</h2>
+          <h3 className="SubTitle"> A Mississippi Brennan Company</h3>
+        </div>
+
       </div>
     </div>
   );
