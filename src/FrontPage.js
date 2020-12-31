5da4f50de0d5cf99415b7b615690789d8d1bbfd4
Cara <cararbrennan@gmail.com>
2020-12-31 11:49:39 -0800

    First commit

---
+++ src/FrontPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import leaf from './images/leaf.png';
+import './FrontPage.css';
+
+function FrontPage() {
+  return (
+    <div className="FrontPage">
+      <div className="FrontPageBody">
+        <h2 className="Title">Haiku Green</h2>
+      </div>
+    </div>
+  );
+}
+
+export default FrontPage;
