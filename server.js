241f9619ed2fae7762aa2b0986ffce457fd232d6
Cara <cararbrennan@gmail.com>
2021-01-17 12:49:19 -0800

    connected to mailchimp signup

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const path = require('path');
 const Mailchimp = require('mailchimp-api-v3');
-require('dotenv').config({ path: __dirname + "/variables.env" });
+require('dotenv').config({ path: __dirname + '/variables.env' });
 
 const mc_api_key = process.env.MAILCHIMP_API_KEY;
-const list_id = process.env.LIST_DI;
+const list_id = process.env.LIST_ID;
 
 const app = express();
 const mailchimp = new Mailchimp(mc_api_key);
