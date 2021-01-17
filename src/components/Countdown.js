bcfac560e53fb25da8231e75c77d83e713f81812
Cara <cararbrennan@gmail.com>
2021-01-16 19:53:17 -0800

    icon and title

---
+++ src/components/Countdown.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import moment, { duration } from 'moment';
+
+//5:32
+
+class Countdown extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+
+        )
+    }
+}
+
+export default Countdown;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/LandingPage.css';
+import Countdown from './Countdown';
+import  Logo  from './Logo';
+import { Title } from './Title';
+import { Description } from './Description';
 
 
+import logo from '../images/logoTransparent.png'
 
-function LandingPage() {
+
+class LandingPage extends Component {
+  state = {
+    logo: {
+      alt: "symmetric flower on a black background that looks like a vulva by VonFlora on Etsy",
+      src: logo,
+    },
+    title: {
+      text:"Haiku Green"
+    },
+    description: {
+      text: "A Mississippi Brennan LLC"
+    }
+  }
+  
+  render(){
+    const {
+      logo,
+      title, 
+      description
+    } = this.state;
+
   return (
     <div className="background">
-    Front Page
+      <Countdown />
+    <Logo alt={logo.alt} src={logo.src}/>
+    <Title text={title.text} />
+    <Description text={description.text}/>
       
     </div>
-  );
+  )};
 }
 
 export default LandingPage;
+
+
